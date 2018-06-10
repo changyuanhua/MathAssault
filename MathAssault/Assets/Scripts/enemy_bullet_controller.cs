fdcde35bc5ef5c1d3d258c32bd571a9a2f13d70f
3Ution-BK <RusTRifull@gmail.com>
2018-06-10 09:46:23 +0800

    UI icon change and bullet scripts structure modify
    We change the UI icon to make it not weird to see.
    
    Previously we forgot about class inheritence, it makes the code harder
    to modify. We first change the bullet scripts to make it easier to
    code. We will modify the other codes in the few days.
    
    * UI icon modify
    * bullet scripts modify

--- MathAssault/Assets/Scripts/enemy_bullet_controller.cs
+++ MathAssault/Assets/Scripts/enemy_bullet_controller.cs
@@ -2,21 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class enemy_bullet_controller : MonoBehaviour {
-    private void Start()
-    {
-        render = GetComponent<Renderer>();
-        movingVector = Vector3.Normalize(Vector3.forward);
-    }
-    private void FixedUpdate()
-    {
-        if (is_moving)
-        {
-            transform.Translate(movingVector * speed * Time.deltaTime);
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
+public class enemy_bullet_controller : bullet
+{
+    protected override void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Environment"))
         {
@@ -29,13 +17,7 @@ public class enemy_bullet_controller : MonoBehaviour {
             is_moving = false;
             render.enabled = false;
             Destroy(gameObject, destroy_time);
-            other.GetComponent<player_controller>().TakingDamage(damage);
+            other.SendMessage("TakingDamage", damage);
         }
     }
-    public int damage = 1;
-    public float speed = 20.0f;
-    public float destroy_time = 1.0f;
-    private Vector3 movingVector;
-    private bool is_moving = true;
-    private Renderer render;
 }
--- MathAssault/Assets/Scripts/player_bullet_controller.cs
+++ MathAssault/Assets/Scripts/player_bullet_controller.cs
@@ -2,22 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class player_bullet_controller : MonoBehaviour
+public class player_bullet_controller : bullet
 {
-    private void Start()
-    {
-        render = GetComponent<Renderer>();
-        movingVector = Vector3.Normalize(Vector3.forward);
-    }
-    private void FixedUpdate()
-    {
-        if (is_moving)
-        {
-            transform.Translate(movingVector * speed * Time.deltaTime);
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
+    protected override void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Environment"))
         {
@@ -32,9 +19,4 @@ public class player_bullet_controller : MonoBehaviour
             Destroy(gameObject, destroy_time);
         }
     }
-    public float speed = 20.0f;
-    public float destroy_time = 1.0f;
-    private Vector3 movingVector;
-    private bool is_moving = true;
-    private Renderer render;
 }
